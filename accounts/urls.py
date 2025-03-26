f257c1aeef36b292221b9ed80ab5318b221df1b5
akagail <ivan.sinaon21@gmail.com>
2025-03-26 22:47:26 +0800

    added functionalities on buttons and javascript

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='parent template'),
-    path('login/', views.login, name='login html'),
-    path('register/', views.register, name='register html'),
+    path('', views.base, name='base'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
