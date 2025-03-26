b0db1a677cbf7cce8f2e935ffab7f2c7d99b122b
akagail <ivan.sinaon21@gmail.com>
2025-03-27 01:32:42 +0800

    added home side panel, made it act on hover

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'capstone',
     'accounts',
+    'home',
 ]
 
 MIDDLEWARE = [
--- capstoneproject/urls.py
+++ capstoneproject/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('capstone.urls')),
     path('', include('accounts.urls')),
+    path('', include('home.urls')),
 ]
